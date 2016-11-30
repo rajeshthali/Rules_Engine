a88473dffae8d5ceef6c11a32e789b924102d693
rajeshthali <rajeshthali@gmail.com>
2016-11-30 18:36:35 +0530

    Latest changes

--- Rules_Engine/web/public/scripts/modules/sample-module/rules-execute-controller.js
+++ Rules_Engine/web/public/scripts/modules/sample-module/rules-execute-controller.js
@@ -5,30 +5,59 @@ define(['angular', './sample-module'], function (angular, controllers) {
          
          function initController() {
         	 $scope.isLoading = true;
+        	 $scope.successExecution = false;
         	 RulesService.getRuleExecuteDetails($stateParams.rulesId,$stateParams.ruleOperator ,function(res){
  				$scope.rulesDetails= angular.copy(res);
  				//create a map for conditionParameter and formulaParameter
         		 $scope.conditionParameterMap = [] // or var map = {};
         		 $scope.formulaParameterMap =[]; // or var map = {};
  				angular.forEach(res.conditionParameter, function(value, key){
- 					var temp = {value:''};
+ 					var temp = {'key': value, 'value':''};
  					$scope.conditionParameterMap.push(temp);
  				});
  				
  				angular.forEach(res.formulaParameter, function(value, key){
- 					//$scope.formulaParameterMap[value] = '';
- 					var temp = {value:''};
- 					$scope.formulaParameterMap.push(temp);
+ 					var temp1 = {'key': value, 'value':''};
+ 					$scope.formulaParameterMap.push(temp1);
 				 });
- 				//map[myKey1] = myObj1;
- 				//map[myKey2] = myObj2;
  				$scope.isLoading = false;
  			  });
  			};
  			
- 			$scope.forwardToEdit= function() {
- 				$state.go('editRule', {rulesId: $stateParams.rulesId});
- 			}
-        
+ 			$scope.cancel = function() {
+ 				$state.go('dashboards');
+ 			};
+ 			
+ 			$scope.ruleExecute = function() {
+ 				//$scope.conditionParameterMap;
+ 				//$scope.formulaParameterMap;
+ 				$scope.isLoading = true;
+ 				var ruleValues = {};
+ 				var serviceParam ={};
+ 				angular.forEach($scope.conditionParameterMap, function(value, key){
+ 					ruleValues[value.key] = value.value;
+ 				});
+ 				if($stateParams.ruleOperator === 'SERVICE') {
+ 					angular.forEach($scope.formulaParameterMap, function(value, key){
+ 						serviceParam[value.key] = value.value;
+ 	 				});
+ 				}else{
+ 					angular.forEach($scope.formulaParameterMap, function(value, key){
+ 	 					ruleValues[value.key] = value.value;
+ 	 				});
+ 				}
+ 				
+ 				RulesService.executeRule($scope.rulesDetails.ruleId,$scope.rulesDetails.ruleName,$scope.rulesDetails.ruleFormulaString,$scope.rulesDetails.conditionFormulaString,$stateParams.ruleOperator,
+ 						$scope.rulesDetails.serviceURL,$scope.rulesDetails.serviceType,ruleValues,serviceParam ,function(res){
+ 				$scope.rulesExecutionDetails= angular.copy(res);
+ 				$scope.successExecution = true;
+ 				$scope.isLoading = false;
+ 			    });
+ 			
+ 			
+	 			$scope.forwardToEdit= function() {
+	 				$state.go('editRule', {rulesId: $stateParams.rulesId});
+	 			};
+ 			};
     }]);
 });
--- Rules_Engine/web/public/scripts/modules/sample-module/rules-service.js
+++ Rules_Engine/web/public/scripts/modules/sample-module/rules-service.js
@@ -25,23 +25,37 @@ define(['angular', './sample-module'], function(angular, sampleModule) {
  					});
                 },
                 getRuleExecuteDetails : function(ruleId,ruleOperator, cb) {
-                	var _paramValues = {
-                			'ruleId':ruleId, 'ruleOperator': ruleOperator
-            			};
-        	          return $http({
-   						method : 'POST',
-   						url : 'https://predix-formula-rule-engine-2.run.aws-usw02-pr.ice.predix.io/rulesEngine/parameterList',
-   						data: angular.toJson(_paramValues),
-   					    headers: {
-                            'Content-Type': 'application/json'
-                        }
-                        
+                	    return $http({
+   						method : 'GET',
+   						url : 'https://predix-formula-rule-engine-2.run.aws-usw02-pr.ice.predix.io/rulesEngine/parameterList/'+ruleId+'/'+ruleOperator,
+   						  
    					}).success(function(response) {
    						}).success(function(response) {
    						if (cb)
    							cb(response);
    					});
-                  }  
+                  },
+                  
+                executeRule: function(ruleId,ruleName,ruleFormulaString,conditionFormulaString,ruleOperator,serviceURL,serviceType,ruleValues,serviceParam, cb) {
+                	var _paramValues = {
+                			'ruleId':ruleId, 'ruleName': ruleName,'ruleFormulaString':ruleFormulaString,'conditionFormulaString':conditionFormulaString,
+                			'ruleOperator':ruleOperator,'serviceURL':serviceURL,'serviceType':serviceType,'ruleValues':ruleValues,'serviceParam':serviceParam};
+                	
+                	console.log(angular.toJson(_paramValues));
+                	
+            		return $http({
+                	  method : 'POST',
+						url : 'https://predix-formula-rule-engine-2.run.aws-usw02-pr.ice.predix.io/rulesEngine/executeRule',
+						data: angular.toJson(_paramValues),
+					    headers: {
+                        'Content-Type': 'application/json'
+                     }
+                	 }).success(function(response) {
+						}).success(function(response) {
+						if (cb)
+							cb(response);
+					});
+                }
          };
     }]);
 });
